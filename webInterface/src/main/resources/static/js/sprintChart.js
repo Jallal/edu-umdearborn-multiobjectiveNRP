59b9500d3d53c0a3ed76f413683b2f7c5f64ed71
Ashley <macbookpro@Macintosh.local>
2019-05-16 22:57:33 -0400

    [Jallal] added more visualization

--- webInterface/src/main/resources/static/js/sprintChart.js
+++ webInterface/src/main/resources/static/js/sprintChart.js
@@ -1,16 +1,27 @@
+google.charts.load("current", {packages: ["timeline"]});
+google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
-        var container = document.getElementById('timeline');
-        var chart = new google.visualization.Timeline(container);
-        var dataTable = new google.visualization.DataTable();
+function drawChart() {
 
-        dataTable.addColumn({ type: 'string', id: 'President' });
-        dataTable.addColumn({ type: 'date', id: 'Start' });
-        dataTable.addColumn({ type: 'date', id: 'End' });
-        dataTable.addRows([
-            [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-            [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-            [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ]]);
+    var container = document.getElementById('timeline');
+    var chart = new google.visualization.Timeline(container);
+    var dataTable = new google.visualization.DataTable();
+    dataTable.addColumn({type: 'string', id: 'Room'});
+    dataTable.addColumn({type: 'string', id: 'Name'});
+    dataTable.addColumn({type: 'date', id: 'Start'});
+    dataTable.addColumn({type: 'date', id: 'End'});
+    dataTable.addRows([
+        ['Task 1', 'Jallal elhazzat', new Date(2019, 5, 1), new Date(2019, 5, 5)],
+        ['Task 2', 'Ashley Snyder', new Date(2019, 5, 1), new Date(2019, 5, 10)],
+        ['Task 3', 'Sri Padadda', new Date(2019, 5, 1), new Date(2019, 5, 12)],
+        ['Task 4', 'Steve Snyder', new Date(2019, 5, 1), new Date(2019, 5, 6)],
+        ['Task 5', 'Jallal elhazzat', new Date(2019, 5, 5), new Date(2019, 5, 12)],
+        ['Task 6', 'Steve Snyder', new Date(2019, 5, 7), new Date(2019, 5, 15)]]
+    );
 
-        chart.draw(dataTable);
-    }
+    var options = {
+        timeline: {colorByRowLabel: true}
+    };
+
+    chart.draw(dataTable, options);
+}
